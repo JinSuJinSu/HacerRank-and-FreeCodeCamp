dbb1c56336860c23521c6243f4e5c6bd2a2185ca
JinSuJinSu <hjs928@naver.com>
2021-03-21 23:54:49 +0900

    Solve this problem about simple mod

---
+++ Python Mod Divmod.py
@@ -0,0 +1,11 @@
+  # Enter your code here. Read input from STDIN. Print output to STDOUT
+  num1 = int(input())
+  num2 = int(input())
+
+  result1 = int(num1/num2)
+  result2 = num1%num2
+  result3 = (result1, result2)
+
+  print(result1)
+  print(result2)
+  print(result3)
