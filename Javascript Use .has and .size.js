15483ff7822e3aabd17495ce6e08464d66cd5c41
JinSuJinSu <hjs928@naver.com>
2021-03-22 20:04:27 +0900

    Solve this problem about simple set functions

---
+++ Javascript Use .has and .size.js
@@ -0,0 +1,16 @@
+  function checkSet(arrToBeSet, checkValue){
+     let result = [];
+     // Only change code below this line
+     if(arrToresultBeSet.has(checkValue)){
+        result.push(true);
+     }
+     else{
+        result.push(false);
+     }
+     result.push(arrToBeSet.size);
+
+     return result;
+
+     // Only change code above this line
+
+  }
